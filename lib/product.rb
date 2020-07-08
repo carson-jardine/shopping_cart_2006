091bfb2bae4062572daf4eca75c53b75a1d7285d
Carson Jardine <carsonjardine@gmail.com>
2020-07-08 09:10:02 -0600

    Add initialize and test

---
+++ lib/product.rb
@@ -0,0 +1,12 @@
+class Product
+  attr_reader :category, :name, :quantity, :unit_price
+
+  def initialize(category, name, unit_price, quantity)
+    @category = category
+    @name = name
+    @quantity = quantity
+    @unit_price = unit_price.round(2)
+  end
+
+  
+end
